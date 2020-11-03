219a70341fd23699df45a40be8af53f09d0cb3ec
Dhivya Sivakumar <dhivyasivakumar1999@gmail.com>
2020-11-03 14:24:01 +0530

    initial

---
+++ proLibManageSys/Data/ModelsContext.cs
@@ -0,0 +1,19 @@
+﻿using proLibManageSys.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace proLibManageSys.Data
+{
+	public class ModelsContext : DbContext
+	{
+		public ModelsContext() : base("DefaultConnection")
+		{ }
+		public DbSet<Books> book { get; set; }
+		public DbSet<Students> student { get; set; }
+		public DbSet<IssuedBooks> issuedBook { get; set; }
+
+	}
+}
